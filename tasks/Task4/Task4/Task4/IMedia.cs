5f420a57e1aab97880a0a59c25abd67fdde52176
pmatula <patrick.matula@gmail.com>
2016-05-21 14:41:20 +0200

    started a new project and implemented my interface class Media

---
+++ tasks/Task4/Task4/Task4/IMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    interface IMedia
+    {
+        string Title { get; }
+
+        Price Price { get; }
+
+        DateTime PublishingDate { get; } 
+    }
+}
